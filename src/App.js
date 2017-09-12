2f626de11686190e2bd2ff2eeb238780fd63ad09
Crazy Lama <lama23@dhcp-10-24-144-111.wlan.ntnu.no>
2017-09-12 15:26:03 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <svg>
+        <circle cx={50} cy={50} r={10} fill="red" />
+      </svg>
     );
   }
 }
