9970c9a6fec028bf6c300032dfbed64d2b754bed
Crazy Lama <lama23@dhcp-10-24-144-111.wlan.ntnu.no>
2017-09-12 15:51:04 +0200

    showing just the axes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import Graph from './script/components/graph'
 
 export default class App extends Component {
   render() {
     return (
-      <svg>
-        <circle cx={50} cy={50} r={10} fill="red" />
-      </svg>
+      <Graph data={this.props.data} />
     );
   }
 }
