06cdd2c3766b200b3a4ccc9cc30edd9e259f3455
Crazy Lama <lama23@dhcp-10-24-144-111.wlan.ntnu.no>
2017-09-13 11:51:58 +0200

    editing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import data from './data/data'
-// import data from './data/one_day'
+// import data from './data/data'
+import data from './data/one_day'
 
 ReactDOM.render(<App datasets={data}/>, document.getElementById('root'));
