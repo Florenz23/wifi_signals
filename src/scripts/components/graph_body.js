6a523951f9a49e33b7c0cba731222c2a5f993fa4
Crazy Lama <lama23@dhcp-10-24-144-167.wlan.ntnu.no>
2017-09-22 14:16:01 +0200

    all graphs timestamp error fixed

--- src/scripts/components/graph_body.js
+++ src/scripts/components/graph_body.js
@@ -9,7 +9,7 @@ export default class GraphBody extends React.Component {
       let date = new Date(timestamp *1000)
       const hour = date.getHours()
       const minutes = date.getMinutes()
-      const new_time = hour + minutes * 0.01
+      const new_time = hour + (minutes/60*100) * 0.01
       return new_time
   }
 
