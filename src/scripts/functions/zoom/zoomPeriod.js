514ca6eeec35e817727c3cd168d9c33a1e28b0fd
Crazy Lama <lama23@dhcp-003125.iet.ntnu.no>
2017-09-29 13:36:12 +0200

    fixed 0 bug and set rssi limit to 15

--- src/scripts/functions/zoom/zoomPeriod.js
+++ src/scripts/functions/zoom/zoomPeriod.js
@@ -1,20 +1,16 @@
 export default function zoomPeriod(data,time,display_timestamps) {
-  console.log(time)
   if( time || time == 0) {
     let keyOfGivenTime = getIndexOfTime(data,time)
-    console.log(keyOfGivenTime)
     let numberOfTimestamps = display_timestamps
     let newArray = cutArray(data,keyOfGivenTime,numberOfTimestamps)
     return newArray
   } else {
-    console.log("moin")
   }
     return data
 }
 
 function getIndexOfTime(data, time) {
   if (!time){
-    console.log("moin")
     return 0
   }
   for (let key in data){
