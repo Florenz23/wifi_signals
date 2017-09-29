a1f34417945b9deb365c095038d27e66f5d0d3c0
Crazy Lama <lama23@dhcp-003125.iet.ntnu.no>
2017-09-29 10:41:04 +0200

    select box is working now

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import Graph from './scripts/components/graph'
 import zoomPeriod from './scripts/functions/zoom/zoomPeriod'
 
+
 export default class App extends React.Component {
-  state = { dataSetIndex: 0 , timeSetIndex:0 }
+  state = { dataSetIndex: 0 , timeSetIndex:0}
   static defaultProps = {
-      views: [["day"],["simpleDay"],["Macs"]],
-      times: [["day"],[0],[2],[4],[6],[8],[10],[12],[14],[16],[18],[20],[22],[24]]
-    }
+    views: [["day"],["simpleDay"],["Macs"]],
+    times: [["day"],[0],[2],[4],[6],[8],[10],[12],[14],[16],[18],[20],[22]]
+
+  }
 
   selectDataset(event) {
     this.setState({dataSetIndex: event.target.value});
@@ -27,8 +29,8 @@ export default class App extends React.Component {
   }
 
   render() {
-    let options_view = this.props.views.map((value, index) => {
-      return <option key={index} value={index}>View {value}</option>
+    let options = this.props.views.map((value, index) => {
+      return <option key={index} value={index}>Ansicht {value}</option>
     });
     let options_time = this.props.times.map((value, index) => {
       return <option key={index} value={index}>Time {value}</option>
@@ -39,12 +41,12 @@ export default class App extends React.Component {
     return (
       <div>
         <select
-          value={this.state.timeSetIndex}
+          value={this.state.dataSetIndex}
           onChange={this.selectDataset.bind(this)} >
-          {options_view}
+          {options}
         </select>
         <select
-          value={this.state.dataSetIndex}
+          value={this.state.timeSetIndex}
           onChange={this.selectTime.bind(this)} >
           {options_time}
         </select>
