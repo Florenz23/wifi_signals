05deeaafc001c47557c379c5300ecb8e86c28439
Crazy Lama <lama23@dhcp-003125.iet.ntnu.no>
2017-10-02 14:07:03 +0200

    got url parameter

--- src/scripts/functions/svg_renderer.js
+++ src/scripts/functions/svg_renderer.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import Graph from '../components/graph'
 
-export default function(data,version) {
+export default function(data,version,time,period) {
 
   return ReactDOMServer.renderToStaticMarkup(<Graph
     data={data}
     viewSelection={version}
+    timeSelection={time}
+    timestampSelection={period}
     />);
 }
--- svg_server.js
+++ svg_server.js
@@ -4,6 +4,7 @@ var app = express();
 
 var fs = require('fs');
 var ndjson = require('ndjson')
+var request = require('request');
 
 // var data = require('./src/data/data').default;
 // let array = []
@@ -20,23 +21,61 @@ var ndjson = require('ndjson')
 //change file here
   var data = require('./src/data/new_files/170915.json');
 
-  var svgRenderer = require('./src/scripts/functions/svg_renderer').default;
+  // var svgRenderer = require('./src/scripts/functions/svg_renderer').default;
+  //
+  // app.get('/svg/0', function (req, res) {
+  //   var svg = svgRenderer(data,0);
+  //   res.send(svg);
+  // });
+  // app.get('/svg/1', function (req, res) {
+  //   var svg = svgRenderer(data,1);
+  //   res.send(svg);
+  // });
+  // app.get('/svg/2', function (req, res) {
+  //   var svg = svgRenderer(data,2);
+  //   res.send(svg);
+  // });
+  app.get('/', function(req, res){
+    console.log(req.query.time)
+    console.log(req.query.moin)
+    res.send('time: ' + req.query.time);
+  });
 
-  app.get('/svg/0', function (req, res) {
-    var svg = svgRenderer(data,0);
-    res.send(svg);
-  });
-  app.get('/svg/1', function (req, res) {
-    var svg = svgRenderer(data,1);
-    res.send(svg);
-  });
-  app.get('/svg/2', function (req, res) {
-    var svg = svgRenderer(data,2);
-    res.send(svg);
-  });
 
   var server = app.listen(4000, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
+    // let urlParameter = readUrl()
+    // console.log(urlParameter)
   });
+
+
+
+  function readUrl() {
+    var url = require('url');
+    console.log(url)
+var url_parts = url.parse(request.url, true);
+var query = url_parts.query;
+var id = req.query.time;
+      console.log(url)
+      return
+      let array = []
+      let time = getParameterByName('time')
+      let period = getParameterByName('period')
+      time = parseInt(time)
+      period = parseInt(period)
+      array = [time,period]
+      return array
+  }
+
+
+  function getParameterByName(name, url) {
+      if (!url) var url = require('url');
+      name = name.replace(/[\[\]]/g, "\\$&");
+      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+          results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return '';
+      return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
